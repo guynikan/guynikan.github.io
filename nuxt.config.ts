a958c5cae408a56a405bd5a12c3c319cc7ee42d2
guynikan <jr-lol@live.com>
2023-02-01 10:50:28 -0300

    add env variables mecanism

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/sanity'],
+  sanity: {
+    projectId: process.env.SANITY_BASE_URL,
+    apiVersion: "2023-01-30"
+
+  },
+
   router: {
     base: '/nuxt-github-pages/'
-  }
+  },
+
+  runtimeConfig: {
+    SANITY_BASE_URL: process.env.SANITY_BASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID
+}
 
 })
