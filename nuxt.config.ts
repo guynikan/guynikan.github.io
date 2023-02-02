a809424030d67a3e98e1e1d9024e0dc41e30243c
guynikan <jr-lol@live.com>
2023-02-02 16:21:16 -0300

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/sanity'],
+  modules: ["@nuxtjs/sanity", "@nuxtjs/tailwindcss"],
   sanity: {
-    projectId: '9ddsm6c7',
-    apiVersion: "2023-01-30"
-
+    projectId: "9ddsm6c7",
+    apiVersion: "2023-01-30",
   },
 
   router: {
-    base: '/nuxt-github-pages/'
+    base: "/nuxt-github-pages/",
   },
 
   runtimeConfig: {
     SANITY_BASE_URL: process.env.SANITY_BASE_URL,
-    PROJECT_ID: process.env.PROJECT_ID
-}
-
-})
+    PROJECT_ID: process.env.PROJECT_ID,
+  },
+});
